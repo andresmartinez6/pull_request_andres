aaa3cafce9c4dcdd85d4db9c45b6e97fa532b237
andresmartinez6 <andresiz11x@gmail.com>
2023-05-11 17:08:29 +0200

    Pull request Andres

--- pullrequest/src/pullrequest/PullRequest.java
+++ pullrequest/src/pullrequest/PullRequest.java
@@ -1,7 +1,32 @@
 package pullrequest;
+
+import java.util.Scanner;
+
 public class PullRequest {
+    
     public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
         System.out.println("Hola soy jose");
+        
+       
+        double num1,num2,suma;
+        
+        System.out.println("Hola soy Andrés");
+        System.out.println("Te hago la siguiente mejora en tu codigo:");
+        System.out.println("-----------------------------------------"
+                + "\nIntroduce 2 numeros y haré la suma");
+        num1=teclado.nextDouble();
+        num2=teclado.nextDouble();
+        
+        suma=num1+num2;
+        
+        if(suma>5){
+            System.out.println("Eres un crak");
+        }else{
+            System.out.println("Eres un matao");
+        }
+        
+        
     }
     
 }
